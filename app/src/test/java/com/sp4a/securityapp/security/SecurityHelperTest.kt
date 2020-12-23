6090e4ee1d9bb988f87c88a69407af0c6879a914
Vaycent <vaycentsun@gmail.com>
2020-12-23 14:41:57 +0800

    Sync

--- app/src/test/java/com/sp4a/securityapp/security/SecurityHelperTest.kt
+++ app/src/test/java/com/sp4a/securityapp/security/SecurityHelperTest.kt
@@ -13,50 +13,35 @@ import org.mockito.junit.MockitoJUnitRunner
 @RunWith(MockitoJUnitRunner::class)
 class SecurityHelperTest {
 
-
     companion object{
         lateinit var context: Context
-        lateinit var mAliSecurityHelper1: SecurityHelper
-        lateinit var mAliSecurityHelper2: SecurityHelper
-        lateinit var mAliSecurityHelper3: SecurityHelper
+        lateinit var mSecurityHelper1: SecurityHelper
+        lateinit var mSecurityHelper2: SecurityHelper
+        lateinit var mSecurityHelper3: SecurityHelper
     }
     @Before
     fun init() {
         println("===@Before init called===")
         context = mock(Context::class.java)
-        mAliSecurityHelper1 = SecurityHelper.getInstance(context)
-        mAliSecurityHelper2 = SecurityHelper.getInstance(context)
-        mAliSecurityHelper3 = SecurityHelper.getInstance(context)
+        mSecurityHelper1 = SecurityHelper.getInstance(context)
+        mSecurityHelper2 = SecurityHelper.getInstance(context)
+        mSecurityHelper3 = SecurityHelper.getInstance(context)
     }
-
-    @Test
-    fun decryptString() {
-        println("===@Test decryptString called===")
-    }
-
-    @Test
-    fun encryptString() {
-        println("===@Test encryptString called===")
-    }
-
     @Test
     fun testInstanceOnce() {
         println("ooo@Test testInstance1 calledooo")
-        assertNotNull(mAliSecurityHelper1)
+        assertNotNull(mSecurityHelper1)
     }
-
     @Test
     fun testInstanceTwice() {
         println("ooo@Test testInstance2 calledooo")
-        assertSame(mAliSecurityHelper1, mAliSecurityHelper2)
+        assertSame(mSecurityHelper1, mSecurityHelper2)
     }
-
     @Test
     fun testInstanceThird() {
         println("ooo@Test testInstance3 calledooo")
-        assertEquals(mAliSecurityHelper3.javaClass.name, mAliSecurityHelper2.javaClass.name)
+        assertEquals(mSecurityHelper3.javaClass.name, mSecurityHelper2.javaClass.name)
     }
-
     @After
     fun clearDataForTest() {
         println("===@After clearDataForTest called===")
